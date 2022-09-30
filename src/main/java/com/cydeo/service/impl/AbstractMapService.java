d54eaa6b6cffb66d8f3a71c732ef97208f83fec9
Kadifa <Kudickadifa@gmail.com>
2022-09-30 09:43:24 -0500

    ticketing project practice

---
+++ src/main/java/com/cydeo/service/impl/AbstractMapService.java
@@ -0,0 +1,29 @@
+package com.cydeo.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractMapService <T,ID>{
+
+    //save in database map(if we have key and value)
+    public Map<ID,T> map=new HashMap<>();
+    T save(ID id,T object){     //save is accepting object and returning object
+    map.put(id,object);
+     return object;
+
+    }
+    List<T> findAll(){
+        return new ArrayList<>(map.values());
+    }
+
+    T findById(ID id){
+        return map.get(id);
+    }
+
+    void deleteById(ID id){
+        map.remove(id);
+    }
+
+}
