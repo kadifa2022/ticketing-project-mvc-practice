e05fcdd284e66e4bbc59bcf3eefa8e49ca193c9b
Kadifa <Kudickadifa@gmail.com>
2022-09-30 09:43:42 -0500

    ticketing project practice

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -5,11 +5,8 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface RoleService {//depends on bus needs
-   RoleDTO save(RoleDTO user);
-   UserDTO findById(Long id);
-   List<UserDTO> findAll();
-   void deleteById(Long id);
+public interface RoleService extends CrudService<RoleDTO,Long>{
+
 
 
 }
