81815fb2acb9b1279777b54fb1a6be8eb599021c
Kadifa <Kudickadifa@gmail.com>
2022-10-01 16:20:13 -0500

    ticketing project practice

--- src/main/java/com/cydeo/convertor/RoleDtoConvertor.java
+++ src/main/java/com/cydeo/convertor/RoleDtoConvertor.java
@@ -7,16 +7,17 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component                    //challenge  that we faced
-@ConfigurationPropertiesBinding //-spring is doing automatically for you just type a
-public class RoleDtoConvertor implements Converter<String, RoleDTO> { //converted String to RoleDTO
-    RoleService roleService;
+@ConfigurationPropertiesBinding //-spring is doing automatically for you
+public class RoleDtoConvertor implements Converter<String, RoleDTO> { //functional converter interface generic //converted String (source)to RoleDTO
+    RoleService roleService;//injections
 
     public RoleDtoConvertor(RoleService roleService) {
         this.roleService = roleService;
     }
 
-    @Override
+    @Override  //generic methode
     public RoleDTO convert(String source) {//"2" go to roleService
-        return roleService.findById(Long.parseLong(source));
+
+        return roleService.findById(Long.parseLong(source));//methode inside class -is going to give you long object instead of string
     }
 }
