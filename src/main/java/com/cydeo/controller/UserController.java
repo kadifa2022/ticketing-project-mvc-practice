a7f8f8c711e476877f4239019f95d5115bf37077
Kadifa <Kudickadifa@gmail.com>
2022-09-30 21:59:58 -0500

    ticketing project practice

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,14 +6,15 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    private final RoleService roleService;
-
-    private final UserService userService;
+    private final RoleService roleService;//interface
+    private final UserService userService;//interface
 
     public UserController(RoleService roleService, UserService userService) {
         this.roleService = roleService;
@@ -22,18 +23,27 @@ public class UserController {
 
 
 
-
-
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles",roleService.findAll());
-        model.addAttribute("user",userService.findAll());
+        model.addAttribute("users",userService.findAll());
 
         return "/user/create";
 
     }
+    @PostMapping ("/create")
+    public String insertUser(@ModelAttribute("user")UserDTO user){
+        // model.addAttribute("user", new UserDTO());
+        //model.addAttribute("roles", roleService.findAll());
+        userService.save(user);
+        // model.addAttribute("users",userService.findAll());
+
+        return"redirect:/user/create";
+
+    }
+
 
 
 
