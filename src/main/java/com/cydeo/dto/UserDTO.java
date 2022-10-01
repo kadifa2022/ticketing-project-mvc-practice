09f08bd236428a609fd857b73c0dee0529dedee8
Kadifa <Kudickadifa@gmail.com>
2022-10-01 16:20:32 -0500

    ticketing project practice

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -16,7 +16,7 @@ public class UserDTO {
     private String passWord;
     private boolean enabled;
     private String phone;
-    private RoleDTO role;
-    private Gender gender;
+    private RoleDTO role;//roleDTO object -WE use convertor to  convert String to role object
+    private Gender gender;//enm
 
 }
