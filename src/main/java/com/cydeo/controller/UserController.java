7b9911fc6ac03cef58e821a06868249566df84b5
Kadifa <Kudickadifa@gmail.com>
2022-10-02 15:18:35 -0500

    ticketing project practice

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,14 +5,12 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    //dependency injection->constructor
     private final RoleService roleService;//interface
     private final UserService userService;//interface
 
@@ -26,22 +24,45 @@ public class UserController {
     @GetMapping("/create")
     public String createUser(Model model){
 
-        model.addAttribute("user", new UserDTO());//need convertor
+        model.addAttribute("user", new UserDTO());//empty user
         model.addAttribute("roles",roleService.findAll());
         model.addAttribute("users",userService.findAll());
 
         return "/user/create";
 
     }
-    @PostMapping ("/create")
-    public String insertUser(@ModelAttribute("user")UserDTO user){
+    @PostMapping ("/create")      //@ModelAttribute holding object//post- methode
+    public String insertUser(@ModelAttribute("user")UserDTO user){//we need to pass empty user object
         // model.addAttribute("user", new UserDTO());
         //model.addAttribute("roles", roleService.findAll());
-        userService.save(user);
+        userService.save(user);//we need to save object -it before
         // model.addAttribute("users",userService.findAll());
 
-        return"redirect:/user/create";
+        return"redirect:/user/create";//why? -application requirement
 
+    }
+    @GetMapping("/update/{username}")//just for retrieving data-need to go to the service
+    public String editUser(@PathVariable("username") String username, Model model){//view and figure what we needed
+
+        model.addAttribute("user", userService.findById(username) );
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+        return"/user/update";
+
+    }
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user")UserDTO user){
+        userService.update(user);
+
+        return"redirect:/user/create";
+    }
+
+    @GetMapping("/delete/{username}")
+    public String  deleteUser(@PathVariable("username") String username){
+        userService.deleteById(username);
+
+        return"redirect:/user/create";
     }
 
 
