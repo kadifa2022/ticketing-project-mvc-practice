76fc168568cb1cf5c16e3927b3dc179c8cd34f3d
Kadifa <Kudickadifa@gmail.com>
2022-10-02 15:17:10 -0500

    ticketing project practice

--- src/main/java/com/cydeo/service/CrudService.java
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -2,11 +2,13 @@ package com.cydeo.service;
 
 import java.util.List;
 
-public interface CrudService <T, ID>{
+public interface CrudService <T, ID>{//all fields are unique
     T save (T user);
     T findById(ID username);
     List<T> findAll();
     void deleteById(ID username);
+
+    void update(T object);
 
 
 }
