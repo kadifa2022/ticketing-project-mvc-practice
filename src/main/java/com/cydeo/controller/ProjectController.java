cc1d005313a3a95a76c321e47416013c29aa3644
Kadifa <Kudickadifa@gmail.com>
2022-10-03 14:19:14 -0500

    ticketing project practice

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,14 +1,13 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project")
@@ -24,18 +23,48 @@ public class ProjectController {
     @RequestMapping("/create")
     public String createProject(Model model){
         model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("managers", userService.findAll());
+        model.addAttribute("managers", userService.findManager());
         model.addAttribute("projects", projectService.findAll());
 
 
         return "/project/create";
     }
     @PostMapping("/create")
-    public String insertProject(@ModelAttribute("project")ProjectDTO project){
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
         projectService.save(project);
-      //  project.setProjectStatus(Status.OPEN);
-
+      //  project.setProjectStatus(Status.OPEN);//we don't put business logic inside controller
 
         return "redirect:/project/create";
     }
+    @GetMapping("/delete/{projectCode}")
+    public String  deleteUser(@PathVariable("projectCode") String projectCode){
+        projectService.deleteById(projectCode);
+
+        return"redirect:/project/create";
+    }
+    @GetMapping("/complete/{projectCode}")
+    public String completeProject(@PathVariable("projectCode") String projectCode){//alleays chec for service
+        projectService.complete(projectService.findById(projectCode));
+
+        return "redirect:/project/create";
+    }
+    @GetMapping("/update/{username}")//pathVariable- parameter                            //just for retrieving data-need to go to the service
+    public String editProject(@PathVariable("username") String projectCode, Model model){//view and figure what we needed
+
+        model.addAttribute("project", projectService.findById(projectCode) );
+        model.addAttribute("managers",userService.findManager());
+        model.addAttribute("projects",projectService.findAll());
+
+        return"/project/update";
+
+    }
+    @PostMapping("/update")//spring is binding @ModelAttribute-Automatically
+    public String updateProject(@ModelAttribute("user") ProjectDTO project){
+        projectService.update(project);//terminology-service
+
+        return"redirect:/project/create";
+    }
+
+
+
 }
