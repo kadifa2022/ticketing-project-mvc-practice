e4032716693c8c9e4ea6e945c08186dfd802bf15
Kadifa <Kudickadifa@gmail.com>
2022-10-03 20:46:06 -0500

    ticketing project practice

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,15 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+
+    public String createTask(Model model) {
+        return "task/create";
+    }
+}
