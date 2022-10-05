2861e313e6b07b31d5a791a36d85d26aeaea46fd
Kadifa <Kudickadifa@gmail.com>
2022-10-05 11:11:49 -0500

    ticketing project practice validation

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,7 +5,10 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -24,25 +27,28 @@ public class UserController {
     @GetMapping("/create")
     public String createUser(Model model){
 
-        model.addAttribute("user", new UserDTO());//empty user-because we need to save
-        model.addAttribute("roles",roleService.findAll());//bring from DB-service-business logic
+        model.addAttribute("user", new UserDTO());             //empty user-because we need to save
+        model.addAttribute("roles",roleService.findAll());      //bring from DB-service-business logic
         model.addAttribute("users",userService.findAll());
 
         return "/user/create";
 
     }
-    @PostMapping ("/create")      //@ModelAttribute holding object//post- methode
-    public String insertUser(@ModelAttribute("user")UserDTO user){//medel is interface -we need to pass empty user object
-        // model.addAttribute("user", new UserDTO());
-        //model.addAttribute("roles", roleService.findAll());
-        userService.save(user);//we need to save object -it before
-        // model.addAttribute("users",userService.findAll());
+    @PostMapping ("/create")                                                            //@ModelAttribute holding object//post- methode
+    public String insertUser(@Valid @ModelAttribute("user")UserDTO user, BindingResult bindingResult, Model model){                 //model is interface -we need to pass empty user object
+       if(bindingResult.hasErrors()){
+           model.addAttribute("roles", roleService.findAll());
+           model.addAttribute("user", userService.findAll());
+           return "/user/create";
+       }
+        userService.save(user);                                //we need to save object -it before
 
-        return"redirect:/user/create";//why? -application requirement
+
+        return"redirect:/user/create";                         //why? -application requirement
 
     }
-    @GetMapping("/update/{username}")//pathVariable- parameter                            //just for retrieving data-need to go to the service
-    public String editUser(@PathVariable("username") String username, Model model){//view and figure what we needed
+    @GetMapping("/update/{username}")                                        //pathVariable- parameter //just for retrieving data-need to go to the service
+    public String editUser(@PathVariable("username") String username, Model model){               //view and figure what we needed
 
         model.addAttribute("user", userService.findById(username) );
         model.addAttribute("roles",roleService.findAll());
@@ -51,8 +57,14 @@ public class UserController {
         return"/user/update";
 
     }
-    @PostMapping("/update")//spring is binding @ModelAttribute-Automatically
-    public String updateUser(@ModelAttribute("user")UserDTO user){
+    @PostMapping("/update")                                                       //spring is binding @ModelAttribute-Automatically
+    public String updateUser(@Valid@ModelAttribute("user")UserDTO user,BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("roles", roleService.findAll());
+            model.addAttribute("user", userService.findAll());
+
+            return "/user/update";
+        }
         userService.update(user);//terminology-service
 
         return"redirect:/user/create";
