b34adaa6b86ebaac655e386d86974576365575fe
Kadifa <Kudickadifa@gmail.com>
2022-10-04 23:43:35 -0500

    ticketing project practice

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -8,7 +8,8 @@ import java.util.List;
 public interface UserService extends CrudService<UserDTO,String>{
 
     //if is something unique for service just add
- List<UserDTO> findManager();
+       List<UserDTO> findManager();
+       List<UserDTO> findEmployees();
 
 
 }
