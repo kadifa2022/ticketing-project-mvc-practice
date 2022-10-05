f6f3d3c5ef071df7dbfeb644c0df7432e5ef7493
Kadifa <Kudickadifa@gmail.com>
2022-10-04 23:43:28 -0500

    ticketing project practice

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -5,16 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO {//limitation
 
     private String firstName;
     private String lastName;
     private String userName;
+    //@NotBlank
+   // @Pattern(regexp = "(?=.\\d)(?=.[a-z])(?=.*[A-Z]).{4,}")
     private String passWord;
     private boolean enabled;
+   // @NotBlank
+  // @Pattern(regexp = "^\\d{10}$")
     private String phone;
     private RoleDTO role;//roleDTO object -WE use convertor to  convert String to role object
     private Gender gender;//enm
