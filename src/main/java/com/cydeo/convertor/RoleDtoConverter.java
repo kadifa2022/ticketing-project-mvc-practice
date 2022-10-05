dfba514d6b3b99d40c5aa12eb7d7126913a2f901
Kadifa <Kudickadifa@gmail.com>
2022-10-05 11:11:37 -0500

    ticketing project practice validation

--- src/main/java/com/cydeo/convertor/RoleDtoConverter.java
+++ src/main/java/com/cydeo/convertor/RoleDtoConverter.java
@@ -6,18 +6,20 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesBindin
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-@Component                    //challenge  that we faced
-@ConfigurationPropertiesBinding //methode get executed-spring is doing automatically for you
-public class RoleDtoConverter implements Converter<String, RoleDTO> { //functional converter interface generic (java-spring) //converted String (source)to RoleDTO
-    RoleService roleService;//get from database RoleService-and then injections
+@Component                                   //challenge  that we faced
+@ConfigurationPropertiesBinding               //methode get executed-spring is doing automatically for you
+public class RoleDtoConverter implements Converter<String, RoleDTO> {             //functional converter interface generic (java-spring) //converted String (source)to RoleDTO
+    RoleService roleService;                                                      //get from database RoleService-and then injections
 
     public RoleDtoConverter(RoleService roleService) {
         this.roleService = roleService;
     }
 
     @Override  //generic methode
-    public RoleDTO convert(String source) {//"2" go to roleService
-                  //go to database and findById
+    public RoleDTO convert(String source) {                                                  //"2" go to roleService
+             if(source==null ||source.equals(" ")) {           //Select
+                 return null;                                                                //go to database and findById
+             }
         return roleService.findById(Long.parseLong(source));  //methode inside class -is going to give you long object instead of string
     }
 }
