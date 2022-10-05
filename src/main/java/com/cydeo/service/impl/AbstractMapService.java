0c6e503772625b007f4ae5a42b4cbe18006e911a
Kadifa <Kudickadifa@gmail.com>
2022-10-04 19:08:53 -0500

    ticketing project practice

--- src/main/java/com/cydeo/service/impl/AbstractMapService.java
+++ src/main/java/com/cydeo/service/impl/AbstractMapService.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class AbstractMapService <T,ID>{//spr
+public abstract class AbstractMapService <T,ID>{//creating custom fake DB and saving-template
 
-    //save in database map(if we have key and value)
+    //save in database map(if we have key and value) //we can create list also
     public Map<ID,T> map=new HashMap<>();//DB//we created abstract class because we don't need to create objects just methode
-    T save(ID id,T object){     //save is accepting object and returning object
+    T save(ID id,T object){     //saving in DB - accepting object and returning object
     map.put(id,object);       //All business logic created
      return object;
 
@@ -25,6 +25,7 @@ public abstract class AbstractMapService <T,ID>{//spr
     void deleteById(ID id){
         map.remove(id);
     }
+
     void update(ID id, T object){
         map.put(id,object);
     }
